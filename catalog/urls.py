1cc00df5b083f8d08d1d00c73f5edf77a8c74923
MitrofanovDm89 <MitrofanovDm89@gmail.com>
2025-07-26 18:12:18 +0300

    Add catalog subcategory pages

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.catalog_index, name='catalog_index'),  # ← вот эта строка
-    path('<slug:slug>/', views.category_detail, name='category_detail'),
+    path('', views.catalog_index, name='catalog_index'),
+    path('huepfburg/', views.huepfburg, name='huepfburg'),
+    path('gesellschaftsspiele/', views.gesellschaftsspiele, name='gesellschaftsspiele'),
+    path('funfood/', views.funfood, name='funfood'),
     path('product/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('<slug:slug>/', views.category_detail, name='category_detail'),
 ]
