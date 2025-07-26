4364ebf2c58159a10c4b82888beaa1126553d3fa
MitrofanovDm89 <MitrofanovDm89@gmail.com>
2025-07-26 18:27:49 +0300

    Add catalog overview with subcategory cards

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.catalog_index, name='catalog_index'),
-    path('huepfburg/', views.huepfburg, name='huepfburg'),
+    path('huepfburgen/', views.huepfburgen, name='huepfburgen'),
     path('gesellschaftsspiele/', views.gesellschaftsspiele, name='gesellschaftsspiele'),
     path('funfood/', views.funfood, name='funfood'),
     path('product/<slug:slug>/', views.product_detail, name='product_detail'),
