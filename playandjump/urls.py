76b46e7bca40d730dbda9dc58779806acedd55f5
MitrofanovDm89 <MitrofanovDm89@gmail.com>
2025-07-26 23:34:21 +0300

    fix: add media URL configuration for serving images in development

--- playandjump/urls.py
+++ playandjump/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),  # главная и статические страницы
     path('katalog/', include('catalog.urls')),  # ← здесь подключаем каталог
     path('warenkorb/', include('cart.urls')),
-
 ]
+
+# Добавляем URL для медиафайлов в режиме разработки
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
