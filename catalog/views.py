b0521980ccc0be4a86ce443770dda2befbcb9576
MitrofanovDm89 <MitrofanovDm89@gmail.com>
2025-07-26 18:34:11 +0300

    Update catalog navigation and pages

--- catalog/views.py
+++ catalog/views.py
@@ -9,8 +9,8 @@ def catalog_index(request):
     return render(request, 'catalog/index.html')
 
 
-def huepfburg(request):
-    return render(request, 'catalog/huepfburg.html')
+def huepfburgen(request):
+    return render(request, 'catalog/huepfburgen.html')
 
 
 def gesellschaftsspiele(request):
