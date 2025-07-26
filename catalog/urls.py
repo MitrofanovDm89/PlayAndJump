324563eeed9105765a3ede6efe9a87e7a24093de
MitrofanovDm89 <MitrofanovDm89@gmail.com>
2025-07-26 16:06:14 +0300

    Fix product detail route order

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.catalog_index, name='catalog_index'),  # ← вот эта строка
-    path('<slug:slug>/', views.category_detail, name='category_detail'),
+    # Маршрут для страницы товара должен располагаться выше,
+    # иначе его "перехватит" маршрут категории
     path('product/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('<slug:slug>/', views.category_detail, name='category_detail'),
 ]
